a4190f2c435156ad97bc04e42f6d911fe05eac35
Charlie Dickens <dickens.c07@gmail.com>
2018-01-25 23:22:06 +0000

    stream operation imprived

--- scripts/initial_test.m
+++ scripts/initial_test.m
@@ -1,13 +1,13 @@
 % Script to test ell_infinity regression with specified block size
-number_of_samples = 5000000 ;
+number_of_samples = 1000 ;
 data = load('data/census_data.mat') ;
 A = data.A(1:number_of_samples,:) ; 
 b = data.b(1:number_of_samples) ;
 X = [A, b] ; 
 feature_idx = 1:size(A,2) ; 
-block_size = 10000 ;
-last_index = block_size ; 
-threshold = size(A,2) / (2*block_size) ; % can be adaptively set for p-norm by how much of index set to keep
+block_size = 100 ;
+end_point = block_size ; 
+threshold = size(A,2) / (block_size) ; % can be adaptively set for p-norm by how much of index set to keep
 
 clear data ; 
 
@@ -29,27 +29,35 @@ B = B(scores>threshold,:) ;
 %     disp(sum(scores)) ; 
 % end
 % 
-while last_index < size(A,1)
+start_point = end_point + 1 ;  % initialise for first loop
+while end_point < size(A,1)
      rows_to_read = block_size - size(B,1) 
      
-     endpoint = min(last_index+1+rows_to_read,size(A,1))   
+     %start_point = last_index+1  
+     end_point = min(start_point+rows_to_read,size(A,1))   
      
      if rows_to_read > 0
-         new_block = X(last_index + 1:endpoint,:) ; 
+         disp('normal reduction')
+         new_block = X(start_point:end_point,:) ; 
          stream_block = [B ; new_block] ; 
          scores = compute_leverage_scores(stream_block(:,feature_idx),2) ; 
-         B = stream_block(scores>threshold,:) ; 
-         size(B) ; 
-         last_index = last_index+1+rows_to_read ; 
+         heavy_list = find(scores>threshold) ; 
+         B = stream_block(heavy_list,:) ; 
+         disp('size(B)')
+         size(B) 
+         start_point = end_point + 1 ; 
+         %last_index = last_index+1+rows_to_read ; 
          
      else
+         disp('storage exceeded reduction') ; 
          scores = compute_leverage_scores(B(:,feature_idx), 2) ; % compute score just on the columns of A
-         B = B(scores>2*threshold,:) ; 
+         B = B(scores>2*threshold,:) ;
+         size(B) ; 
      end
      
-     last_index = last_index + block_size ; 
-     last_index 
-     endpoint 
+     %last_index = last_index + block_size ; 
+     %last_index 
+     end_point 
 %     if rows_to_read == 0
 %         %reduce(B)
 %     else
